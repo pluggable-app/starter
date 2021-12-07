56e734fc4740ad02ae8c52308c3f5a556eaf47af
Mujib <mujib517@gmail.com>
2021-12-07 17:57:32 +0530

    include prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -6,33 +6,35 @@ module.exports = {
         extensions: ['.js', '.jsx', '.json', '.scss']
     },
     module: {
-        rules: [{
-            test: /\.(js|jsx)?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        },
-        {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-        },
-        {
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
-        },
-        {
-            test: /\.(png|gif|jpg|jpeg)$/,
-            use: ["file-loader"]
-        }]
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|gif|jpg|jpeg)$/,
+                use: ['file-loader']
+            }
+        ]
     },
     devServer: {
         port: 9000,
         historyApiFallback: {
-            index: 'index.html',
-        },
+            index: 'index.html'
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './public/index.html',
+            template: './public/index.html'
         })
     ]
 }
