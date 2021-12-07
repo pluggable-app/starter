27a6b2bf2f51194197a79729569ab37bda70c7fa
Mujib <mujib517@gmail.com>
2021-12-07 18:04:18 +0530

    change template from html to ejs

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,46 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = {
-    mode: 'development',
-    resolve: {
-        extensions: ['.js', '.jsx', '.json', '.scss']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(png|gif|jpg|jpeg)$/,
-                use: ['file-loader']
+module.exports = (webpackConfigEnv, argv) => {
+    return {
+        mode: 'none',
+        resolve: {
+            extensions: ['.js', '.jsx', '.json', '.scss']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(png|gif|jpg|jpeg)$/,
+                    use: ['file-loader']
+                }
+            ]
+        },
+        devServer: {
+            port: 9000,
+            historyApiFallback: {
+                index: 'index.html'
             }
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(webpackConfigEnv.NODE_ENV)
+            }),
+            new HtmlWebpackPlugin({
+                template: './src/index.ejs'
+            })
         ]
-    },
-    devServer: {
-        port: 9000,
-        historyApiFallback: {
-            index: 'index.html'
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
+    }
 }
